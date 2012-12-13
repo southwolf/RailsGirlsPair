6a54c0d3e617b9b18bc0abd1dcecc320e85e1d0d
sundevilyang <kevin.wenyang@gmail.com>
2012-12-13 16:14:48 +0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsGirlsPair::Application.config.secret_token = 'cadccdbce28feea5d669107fc0664abe7a39e313e57069779f3ca844bbd405ff9cf56079fa59c8afbec65923c713ab7201f1fb4dce5512847f325d3f0f22da7e'
